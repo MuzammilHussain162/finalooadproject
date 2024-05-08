b2925b258805d0a11246db523847e2687d46e9c3
Admin <Admin@MUZAMMILHUSSAIN>
2024-05-08 09:48:40 +0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,18 @@
+namespace finalooadproject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.Hide();
+        }
+
+    }
+}
---
+++ loginform.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace finalooadproject
+{
+    public partial class loginform : Form
+    {
+        public loginform()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_projectheading_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linklbl_loginasemployee_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            loginform lgmanger = new loginform();
+            lgmanger.Show();
+        }
+    }
+}
